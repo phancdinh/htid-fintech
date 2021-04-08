fa67936b120e06a57d256637f889ad53ddd29968
dinh.phan <congdinhphan@gmail.com>
2021-04-08 10:49:57 +0700

    Change project uri

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ export const CONFIG = {
     LOGOUT_URL: `${HOST}/oidc/logout`,
     RESPONSE_TYPE: "code",
     SCOPE: "openid profile phone",
-    REDIRECT_URI: "https://topen-fintech.herokuapp.com",
+    REDIRECT_URI: "https://topen-fintech.azurewebsites.net",
 
     // id of 'Demo - SPA' Service Provider
     CLIENT_ID: "BWIItvzyes_2FFnQCtQkjspAtvQa",
@@ -23,7 +23,7 @@ export const CONFIG = {
     COOKIE_PATH: "/",
 
     // endpoint of User Profile service
-    PROFILE_SERVICE_URL: "https://profile-demo-ht.herokuapp.com",
+    PROFILE_SERVICE_URL: "https://profile-demo-ht.azurewebsites.net",
 
     APIM_URL: "http://api-profile-dev.hungthinhcorp.com.vn:8080",
     // consumer key of API subscription on devportal
